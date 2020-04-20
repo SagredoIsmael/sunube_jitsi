23275ac82bd74cdb02300d00a64331fc12954976
SagredoIsmael <sagredo.ismael@live.com>
2020-04-20 14:36:29 +0200

    add Main screen

---
+++ src/screens/Main.js
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import { View, Text } from 'react-native'
+
+export default ({navigation}) =>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Main Screen</Text>
+    </View>
