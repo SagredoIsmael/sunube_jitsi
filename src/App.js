9737fdeb6d756eb796bc013bc8eda8f1a1e64131
SagredoIsmael <sagredo.ismael@live.com>
2020-04-20 14:55:01 +0200

    fix import navigationContainer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import Navigation from './screens/Navigation'
 
@@ -7,6 +7,3 @@ export default () =>
 <NavigationContainer>
   <Navigation/>
 </NavigationContainer>
-
-
-export default App
--- src/screens/Navigation.js
+++ src/screens/Navigation.js
@@ -1,29 +1,30 @@
 import * as React from 'react'
 import MainScreen from './Main'
-import JitsiMeetScreen from './JitsiMeet'
+import MeetScreen from './Meet'
 import { createStackNavigator } from '@react-navigation/stack'
+import { colors, screens } from '../utils/constants'
 
+const { MAIN, MEET } = screens
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 function App() {
-  return (
-    <NavigationContainer>
-        <Stack.Navigator initialRouteName={"MAIN"} screenOptions={headerCommonsOptions}>
-        <Stack.Screen name="MAIN" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+    return (
+        <Stack.Navigator initialRouteName={MAIN} screenOptions={headerCommonsOptions}>
+            <Stack.Screen name={MAIN} component={MainScreen} />
+            <Stack.Screen name={MEET} component={MeetScreen} />
+        </Stack.Navigator>
+    )
 }
 
 const headerCommonsOptions = {
     headerStyle: {
-        backgroundColor: UIStyles.blueColor,
+        backgroundColor: colors.background,
     },
-    headerTintColor: UIStyles.vomitColor,
+    headerTintColor: colors.primary,
     headerTitleStyle: {
         fontWeight: 'bold',
     },
 }
 
-export default App;
+export default App
