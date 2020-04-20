e37261c6b55b66cbdca5fbb33747cdcadb415f42
SagredoIsmael <sagredo.ismael@live.com>
2020-04-20 16:40:28 +0200

    add styles

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import 'react-native-gesture-handler'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import Navigation from './screens/Navigation'
+import { StatusBar } from 'react-native'
 
 export default () => 
 <NavigationContainer>
+<StatusBar hidden />
   <Navigation/>
 </NavigationContainer>
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,35 +1,72 @@
 import * as React from 'react'
-import { Button, Text, Image, StyleSheet } from 'react-native'
+import { TouchableOpacity, Text, Image, StyleSheet, TextInput } from 'react-native'
 import { screens, colors } from '../utils/constants'
 import LinearGradient from 'react-native-linear-gradient'
-
 const { MEET } = screens
 
 
-export default ({ navigation }) =>
-    <LinearGradient colors={colors.gradient} style={styles.container}>
-        <Image style={styles.waterMark} source={require('../../img/watermark.png')} />
+export default ({ navigation }) => {
+    const [textNameMeet, onChangeText] = React.useState('')
+    return (
+        <LinearGradient colors={colors.gradient} style={styles.container}>
+            <Image style={styles.waterMark} source={require('../../img/watermark.png')} />
+            <Text style={styles.text}>Seguro, lleno de funcionalidades y videoconferencias completamente gratuitas</Text>
+            <TextInput
+                style={styles.inputText}
+                onChangeText={text => onChangeText(text)}
+                value={textNameMeet}
+                placeholder={"Comenzar una reunión"}
+                placeholderTextColor={'white'}
+            />
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate(MEET)} underlayColor={colors.secondary}>
+                <Text style={styles.textButton}>IR</Text>
+            </TouchableOpacity>
+           
+        </LinearGradient>
+    )
+}
 
-        <Text>Main Screen</Text>
-        <Button
-            title="Go to Details"
-            onPress={() => navigation.navigate(MEET)}
-        />
-    </LinearGradient>
 
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
+        alignItems: 'center'
     },
     waterMark: {
-        marginTop: '25%',
+        marginTop: '20%',
         width: '80%',
         height: '20%',
     },
-    logo: {
-        width: 66,
-        height: 58,
+    text: {
+        textAlign: 'center',
+        margin: '10%',
+        color: 'white',
+        fontWeight: "bold",
+        fontSize: 17
     },
+    textButton: {
+        textAlign: 'center',
+        margin: '5%',
+        color: colors.primary,
+        fontWeight: "bold",
+        fontSize: 17
+    },
+    inputText: {
+        width: '75%',
+        marginBottom: '10%',
+        borderColor: 'white',
+        borderWidth: 2,
+        textAlign: 'center',
+        fontSize: 17,
+        color: 'white'
+    },
+    button: {
+        backgroundColor: 'white',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#fff'
+    }
 });
